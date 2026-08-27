15a3dc2a47cf8f0c69167144ebe573422e3415cd
Danilo Camargo <danilo@example.com>
2026-08-27 18:09:23 +0000

    fix: revert to java 21 compatible versions (kotlin 1.9.23, spring boot 3.2.4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
-	kotlin("jvm") version "2.0.0"
-	kotlin("plugin.spring") version "2.0.0"
-	id("org.springframework.boot") version "3.4.0"
-	id("io.spring.dependency-management") version "1.1.6"
+	kotlin("jvm") version "1.9.23"
+	kotlin("plugin.spring") version "1.9.23"
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.danilo"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_21
-
-kotlin {
-	jvmToolchain(21)
-}
 
 repositories {
 	mavenCentral()
