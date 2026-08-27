f35a4a2e2e2ebfb88fb42c96a5ad86c7687941b6
Danilo Camargo <danilo@example.com>
2026-08-27 18:28:10 +0000

    fix: update kotlin to 1.9.24 and spring boot to 3.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.danilo"
